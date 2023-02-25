9a8093b7090b8415902a3b8b246aa35483045582
WYF <w.y.ff@163.com>
2023-02-25 16:38:54 +0800

    update ci

--- auto.sh
+++ auto.sh
@@ -8,7 +8,11 @@ READWISE_TOKEN=$1
 ZOTERO_KEY=$2
 ZOTERO_ID=$3
 
-python run.py $READWISE_TOKEN $ZOTERO_KEY $ZOTERO_ID --version_number=12799 >> /tmp/readwise.log 2>&1
+VERSION_NUMBER=$(cat ./zotero_version_number)
+
+python run.py $READWISE_TOKEN $ZOTERO_KEY $ZOTERO_ID --version_number=${VERSION_NUMBER} >> /tmp/readwise.log 2>&1
+
+cat /tmp/readwise.log
 
 tail -n 1 /tmp/readwise.log > ./zotero_version_number
 
