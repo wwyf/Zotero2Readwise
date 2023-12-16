37f437e246760b1c632c4e9e813374da15238491
wwyf <w.y.ff@163.com>
2023-12-16 23:00:57 +0800

    check the log

--- auto.sh
+++ auto.sh
@@ -16,6 +16,15 @@ python run.py $READWISE_TOKEN $ZOTERO_KEY $ZOTERO_ID --version_number=${VERSION_
 
 cat /tmp/readwise.log
 
+log_tail=`tail -n 1 /tmp/readwise.log`
+
+echo $log_tail
+
+re='^[0-9]+$'
+if ! [[ $log_tail =~ $re ]] ; then
+   echo "error: Not a number" >&2; exit 1
+fi
+
 tail -n 1 /tmp/readwise.log > ./zotero_version_number
 
 echo new version number
